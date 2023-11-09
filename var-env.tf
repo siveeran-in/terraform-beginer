85cab142023ecb708d4c49f7bc74b73bc4fefcb3
siveeran-in <146727053+siveeran-in@users.noreply.github.com>
2023-11-09 22:16:57 +0530

    Add files via upload

---
+++ var-env.tf
@@ -0,0 +1,35 @@
+variable "client_id" {
+  default = "0c092783-d289-4522-adc7-fceda9876c9b"
+}
+
+variable "client_secret" {
+  default = "pxT8Q~AZZhvINCLJhD2T8.PTK9ymnKNoYu8LvcEZ"
+}
+
+variable "tenant_id" {
+  default = "6a69f483-9714-4eb3-aedc-ef87d3497470"
+}
+
+variable "subscription_id" {
+  default = "00144ea6-e433-44c4-bbd2-6d75d72bc05d"
+}
+
+variable "prefix" {
+  default = "siva"
+}
+
+variable "location" {
+  default = "Central India"
+}
+
+variable "size" {
+  default = "Standard_F2"
+}
+
+variable "admin_username" {
+  default = "adminuser"
+}
+
+variable "admin_password" {
+  default = "P@ssw0rd1"
+}
